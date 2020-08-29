47fe67e917fe993ef95d652e1ed1f993fa6a4444
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-29 18:56:25 -0500

    Added logic to retrieve user data by id (sans password) by auth route.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,9 +1,19 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
+const User = require('../../models/User');
 
 //@route   GET api/auth
 //@desc    Test route
 //@access  Public
-router.get('/', (req, res) => res.send('Auth route'));
+router.get('/', auth, async (req, res) => {
+  try {
+    //.select('-password') makes sure the password is not returned
+    const user = await User.findById(req.user.id).select('-password');
+    res.json(user);
+  } catch (err) {
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
