6fad378c5d1a8bea73abb0515ace2413af366fc5
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-29 20:06:37 -0500

    Added logic for user to login including comparing passwords.

--- routes/api/auth.js
+++ routes/api/auth.js
@@ -1,10 +1,14 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
 const auth = require('../../middleware/auth');
+const jwt = require('jsonwebtoken');
+const config = require('config');
+const { check, validationResult } = require('express-validator');
 const User = require('../../models/User');
 
 //@route   GET api/auth
-//@desc    Test route
+//@desc    Return user information
 //@access  Public
 router.get('/', auth, async (req, res) => {
   try {
@@ -15,5 +19,66 @@ router.get('/', auth, async (req, res) => {
     res.status(500).send('Server Error');
   }
 });
+
+//@route   POST api/auth
+//@desc    Authenticate user & get token
+//@access  Public
+router.post(
+  '/',
+  [
+    check('email', 'Please include a valid email').isEmail(),
+    check('password', 'Password is required').exists(),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      return res.status(400).json({ errors: errors.array() });
+    }
+
+    //destructure req.body
+    const { email, password } = req.body;
+
+    try {
+      //See if user exists
+      //If user exists send back error (duplicate)
+      let user = await User.findOne({ email });
+
+      if (!user) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //use bcrypt to make sure password is correct (compare)
+      const isMatch = await bcrypt.compare(password, user.password);
+
+      if (!isMatch) {
+        return res
+          .status(400)
+          .json({ errors: [{ msg: 'Invalid Credentials' }] });
+      }
+
+      //Return jsonwebtoken (so after registration user can be logged in immediately)
+      const payload = {
+        user: {
+          id: user.id, //mongoose abstracts away the _id format on mongoDB to just id
+        },
+      };
+
+      jwt.sign(
+        payload,
+        config.get('jwtSecret'),
+        { expiresIn: 360000 },
+        (err, token) => {
+          if (err) throw err;
+          res.json({ token });
+        }
+      );
+    } catch (err) {
+      console.error(err.message);
+      res.status(500).send('Server error');
+    }
+  }
+);
 
 module.exports = router;
