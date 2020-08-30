beea26f9e81d67782d5bf6e560df711288657790
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-30 14:38:18 -0500

    Using async await (in particular b/c Mongoose returns promises) created route to get current user's profile.

--- routes/api/profile.js
+++ routes/api/profile.js
@@ -1,9 +1,29 @@
 const express = require('express');
 const router = express.Router();
+const auth = require('../../middleware/auth');
 
-//@route   GET api/profile
-//@desc    Test route
-//@access  Public
-router.get('/', (req, res) => res.send('Profile route'));
+const Profile = require('../../models/Profiles');
+const User = require('../../models/User');
+const Profiles = require('../../models/Profiles');
+
+//@route   GET api/profile/me
+//@desc    Get current user's profile
+//@access  Private
+router.get('/me', auth, async (req, res) => {
+  try {
+    const profile = await Profiles.findOne({
+      user: req.user.id,
+    }).populate('user', ['name', 'avatar']);
+
+    if (!profile) {
+      return res.status(400).json({ msg: 'There is no profile for this user' });
+    }
+
+    res.json(profile);
+  } catch (err) {
+    console.error(err.message);
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
