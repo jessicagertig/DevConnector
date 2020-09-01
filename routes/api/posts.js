449eebc95237ee93d7323c332061682eec1e1aee
Jessica Cooper <jessicagcooper@gmail.com>
2020-08-31 20:12:57 -0500

    Add route to get post by post id.

--- routes/api/posts.js
+++ routes/api/posts.js
@@ -1,9 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const {
-  check,
-  validationResult
-} = require('express-validator');
+const { check, validationResult } = require('express-validator');
 const auth = require('../../middleware/auth');
 
 const Post = require('../../models/Post');
@@ -47,13 +44,34 @@ router.post(
 // @access  Private
 router.get('/', auth, async (req, res) => {
   try {
-    const posts = await Post.find().sort({ date: -1 })// -1 returns most recent posts first, 1 would return oldest first
+    const posts = await Post.find().sort({ date: -1 }); // -1 returns most recent posts first, 1 would return oldest first
 
-    res.json(posts)
+    res.json(posts);
   } catch (err) {
     console.error(err.message);
     res.status(500).send('Server Error');
   }
-})
+});
+
+// @route   GET api/posts/:id
+// @desc    Get post by id
+// @access  Private
+router.get('/:id', auth, async (req, res) => {
+  try {
+    const post = await Post.findById(req.params.id)
+
+    if(!post) {
+      return res.status(404).json({ msg: 'Post not found'})
+    }
+
+    res.json(post);
+  } catch (err) {
+    console.error(err.message);
+    if(err.kind === 'ObjectId') {
+      return res.status(404).json({ msg: 'Post not found'})
+    }
+    res.status(500).send('Server Error');
+  }
+});
 
 module.exports = router;
