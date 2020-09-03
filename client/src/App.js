3ef555256e829904fc62a6c977dd2128da325050
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-02 21:39:41 -0500

    Pulled in react-router-dom and wrap App in router. Create route to Landing.

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,16 @@
 import React, { Fragment } from 'react';
-
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './components/layout/NavBar'
 import Landing from './components/layout/Landing'
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <NavBar />
-    <Landing />
-  </Fragment>
+  <Router>
+    <Fragment>
+      <NavBar />
+      <Route exact path='/' component={ Landing } />
+    </Fragment>
+  </Router>
 )
 
 export default App;
