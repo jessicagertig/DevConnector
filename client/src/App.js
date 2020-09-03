adb809a76cf5f89116ff7a772ab55a34d2b54ca1
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-03 00:00:08 -0500

    Added scaffolding for auth components.

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,9 @@ import React, { Fragment } from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 import NavBar from './components/layout/NavBar'
 import Landing from './components/layout/Landing'
+import Register from './components/auth/Register'
+import Login from './components/auth/Login'
+
 import './App.css';
 
 const App = () => (
@@ -9,6 +12,12 @@ const App = () => (
     <Fragment>
       <NavBar />
       <Route exact path='/' component={ Landing } />
+      <section className="container">
+        <Switch>
+          <Route exact path="/register" component={Register} />
+          <Route exact path="/login" component={Login} />
+        </Switch>
+      </section>
     </Fragment>
   </Router>
 )
