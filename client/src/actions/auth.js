fbec4b06b1db34f61d8e472c942db71393be25ee
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-06 01:38:20 -0500

    Added error handling to register auth action.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -1,4 +1,5 @@
 import axios from 'axios'
+import { setAlert } from './alert'
 import {
   REGISTER_SUCCESS,
   REGISTER_FAIL
@@ -22,6 +23,10 @@ export const register = ({ name, email, password }) => async dispatch => {
       payload: res.data//the token
     })
   } catch (err) {
+    const errors = err.response.data.errors
+    if(errors){
+      errors.forEach(error => dispatch(setAlert(error.msg, 'danger')))
+    }
     dispatch({
       type: REGISTER_FAIL
     })
