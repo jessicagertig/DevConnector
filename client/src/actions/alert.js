6f3f0a7f5f213d0d31ed9cd46b8372977d9383d1
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-05 21:04:26 -0500

    Create first basic reducers(alert), action, install dep uuid.

---
+++ client/src/actions/alert.js
@@ -0,0 +1,13 @@
+import uuid from 'uuid';
+import { SET_ALERT, REMOVE_ALERT } from './types';
+
+//need the thunk middleware in order to use dispatch in this way (basically bringing in the dispatch function)
+export const setAlert = (msg, alertType) => dispatch => {
+  //uuid package allows us to generate random ids
+  const id = uuid.v4();
+  //dispatch is calling SET_ALERT
+  dispatch({
+    type: SET_ALERT,
+    payload: { msg, alertType, id },
+  });
+};
---
+++ client/src/reducers/alert.js
@@ -0,0 +1,20 @@
+//a reducer takes in state and an action
+import { SET_ALERT, REMOVE_ALERT } from '../actions/types'
+
+const initialState = []
+
+export default function(state = initialState, action) {
+  //desconstruct action.type and action.payload
+  const { type, payload } = action;
+
+  switch(type) {
+    case SET_ALERT:
+      //since state is immutable we want to include any state that is already there(with spread operator) and then add our new alert
+      return [...state, payload];
+    case REMOVE_ALERT:
+      //for REMOVE_ALERT the payload will only be the alert ID, we'll filter through alerts and return all except for the one matching the payload/ID
+      return state.filter(alert => alert.id !== payload);
+    default:
+      return state;
+  }
+}
