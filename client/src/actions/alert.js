fc1ed433a1600fb3b362a33172b4a574511f5f83
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-05 22:04:39 -0500

    Added ability to pass in time parameter to setAlert, but set default so don't have to.

--- client/src/actions/alert.js
+++ client/src/actions/alert.js
@@ -2,7 +2,7 @@ import { v4 as uuidv4 } from 'uuid';
 import { SET_ALERT, REMOVE_ALERT } from './types';
 
 //need the thunk middleware in order to use dispatch in this way (basically bringing in the dispatch function)
-export const setAlert = (msg, alertType) => dispatch => {
+export const setAlert = (msg, alertType, timeout = 5000) => dispatch => {
   //uuid package allows us to generate random ids
   const id = uuidv4();
   //dispatch is calling SET_ALERT
@@ -11,5 +11,5 @@ export const setAlert = (msg, alertType) => dispatch => {
     payload: { msg, alertType, id },
   });
   //this sets up the alert to be removed after 5 seconds
-  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), 5000); //5000 is 5 seconds
+  setTimeout(() => dispatch({ type: REMOVE_ALERT, payload: id }), timeout); //5000 is 5 seconds
 };
