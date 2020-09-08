b3cc421b82f23c9ea7ec7112b8507fbb96e7d8a3
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-08 00:55:07 -0500

    Removed 'config' from api requests.

--- client/src/actions/auth.js
+++ client/src/actions/auth.js
@@ -8,7 +8,6 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
 } from './types';
-import setAuthToken from '../utils/setAuthToken';
 
 //Load USER
 export const loadUser = () => async dispatch => {
@@ -33,7 +32,7 @@ export const register = ({ name, email, password }) => async dispatch => {
   const body = JSON.stringify({ name, email, password });
 
   try {
-    const res = await axios.post('/api/users', body, config);
+    const res = await axios.post('/api/users', body);
 
     dispatch({
       type: REGISTER_SUCCESS,
@@ -57,7 +56,7 @@ export const login = (email, password) => async dispatch => {
   const body = JSON.stringify({ email, password });
 
   try {
-    const res = await axios.post('/api/auth', body, config);
+    const res = await axios.post('/api/auth', body);
 
     dispatch({
       type: LOGIN_SUCCESS,
