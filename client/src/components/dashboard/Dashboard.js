9bdbcbb1f430103eab17037852ca5d72c7224ded
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-08 02:32:42 -0500

    Created PrivateRoute component and made Dashboard a PrivateRoute.

--- client/src/components/dashboard/Dashboard.js
+++ client/src/components/dashboard/Dashboard.js
@@ -1,16 +1,10 @@
-import React from 'react'
-import PropTypes from 'prop-types'
+import React from 'react';
+import PropTypes from 'prop-types';
 
 const Dashboard = () => {
-  return (
-    <div>
-      Dashboard
-    </div>
-  )
-}
+  return <div>Dashboard</div>;
+};
 
-Dashboard.propTypes = {
-  
-}
+Dashboard.propTypes = {};
 
-export default Dashboard
+export default Dashboard;
