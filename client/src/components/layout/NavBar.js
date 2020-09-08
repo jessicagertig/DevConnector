aa4d7c47cd829120daa1ddfd2ed71d5e415993f5
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-08 01:55:40 -0500

    Add two different NavBar by manipulating classname & using ternary in NavBar component.  Add logout icon.

--- client/src/components/layout/NavBar.js
+++ client/src/components/layout/NavBar.js
@@ -1,19 +1,56 @@
-import React from 'react'
-import { Link } from 'react-router-dom'
+import React, { Fragment } from 'react';
+import { Link } from 'react-router-dom';
+import { connect } from 'react-redux';
+import PropTypes from 'prop-types';
+import { logout } from '../../actions/auth';
 
-const NavBar = () => {
-  return (
-    <nav className="navbar bg-dark">
-    <h1>
-      <Link to='/'><i className="fas fa-code"></i> DevConnector</Link>
-    </h1>
+const NavBar = ({ auth: { isAuthenticated, loading }, logout }) => {
+  const authLinks = (
     <ul>
-      <li><Link href="profiles.html">Developers</Link></li>
-      <li><Link to='/register'>Register</Link></li>
-      <li><Link to='/login'>Login</Link></li>
+      <li>
+        <a onClick={logout} href='#!'>
+          <i className='fas fa-sign-out-alt'></i>{' '}
+          <span className='hide-sm'>Logout</span>
+        </a>
+      </li>
     </ul>
-  </nav>
-  )
-}
+  );
 
-export default NavBar
+  const guestLinks = (
+    <ul>
+      <li>
+        <a href='#!'>Developers</a>
+      </li>
+      <li>
+        <Link to='/register'>Register</Link>
+      </li>
+      <li>
+        <Link to='/login'>Login</Link>
+      </li>
+    </ul>
+  );
+
+  return (
+    <nav className='navbar bg-dark'>
+      <h1>
+        <Link to='/'>
+          <i className='fas fa-code'></i> DevConnector
+        </Link>
+      </h1>
+      {!loading && (
+        <Fragment>{isAuthenticated ? authLinks : guestLinks}</Fragment>
+      )}
+    </nav>
+  );
+};
+
+NavBar.propTypes = {
+  logout: PropTypes.func.isRequired,
+  auth: PropTypes.object.isRequired,
+};
+
+const mapStateToProps = state => ({
+  auth: state.auth,
+});
+
+export default connect(mapStateToProps, { logout })(NavBar);
