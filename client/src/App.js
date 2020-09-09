f5a0bf04f544e9dd912913ba0400a2c3c3a80834
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-09 01:18:09 -0500

    CreateProfile form componenet set up and added as route.

--- client/src/App.js
+++ client/src/App.js
@@ -5,8 +5,9 @@ import Landing from './components/layout/Landing';
 import Register from './components/auth/Register';
 import Login from './components/auth/Login';
 import Alert from './components/layout/Alert';
-import Dashboard from './components/dashboard/Dashboard'
-import PrivateRoute from './components/routing/PrivateRoute'
+import Dashboard from './components/dashboard/Dashboard';
+import PrivateRoute from './components/routing/PrivateRoute';
+import CreateProfile from './components/profile-forms/CreateProfile';
 //REDUX
 import { Provider } from 'react-redux';
 import store from './store';
@@ -18,7 +19,7 @@ import './App.css';
 const App = () => {
   useEffect(() => {
     //check for token in LS and load user
-    if (localStorage.token){
+    if (localStorage.token) {
       setAuthToken(localStorage.token);
       store.dispatch(loadUser());
     }
@@ -37,6 +38,11 @@ const App = () => {
               <Route exact path='/register' component={Register} />
               <Route exact path='/login' component={Login} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute
+                exact
+                path='/create-profile'
+                component={CreateProfile}
+              />
             </Switch>
           </section>
         </Fragment>
