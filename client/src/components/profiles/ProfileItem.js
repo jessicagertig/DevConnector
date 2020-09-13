2565357ba820583dd06f61221c31792063a6f141
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-12 22:15:23 -0500

    Start to set up public display of profiles.

---
+++ client/src/components/profiles/ProfileItem.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileItem = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ProfileItem.propTypes = {
+
+}
+
+export default ProfileItem
