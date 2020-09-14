cdd964777bf816821f7186c7597cdc7658061529
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-14 01:24:37 -0500

    Added profiles display.  Tried to fix/update auth re github token and gravatar.  Gravatar ot showing.

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -2,6 +2,7 @@ import React, { Fragment, useEffect } from 'react';
 import PropTypes from 'prop-types';
 import { connect } from 'react-redux';
 import Spinner from '../layout/Spinner';
+import ProfileItem from '../profiles/ProfileItem'
 import { getProfiles } from '../../actions/profile';
 
 const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
