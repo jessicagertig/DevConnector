75b1a0469fbd0ef6dc616bf8373ce8860391a563
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-14 00:42:50 -0500

    Set up Profiles to component to show data (from ProfileItem) IF profile array > 0.

--- client/src/components/profiles/Profiles.js
+++ client/src/components/profiles/Profiles.js
@@ -12,9 +12,20 @@ const Profiles = ({ getProfiles, profile: { profiles, loading } }) => {
   console.log('profiles', profiles)
 
   return <Fragment>
-    { loading ? <Spinner /> : <Fragment>
-      
-      </Fragment>};
+    { loading ? <Spinner/> : <Fragment>
+        <h1 className="large text-primary">Developers</h1>
+        <p className="lead">
+          <i className="fab fa-connectdevelop"></i> Browse and connect with developers
+        </p>
+        <div className="profiles">
+          {profiles.length > 0 ? (
+            profiles.map(profile => (
+              <ProfileItem key={profile._id} profile={profile} />
+            ))
+          ) : <h4>No profiles found...</h4>}
+        </div>
+      </Fragment> }
+    </Fragment>;
 };
 
 Profiles.propTypes = {
