a326870699e7d650b01116face2717f219dddd53
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-14 01:34:09 -0500

    Updated more code.  Fixed type re gravatar.

--- client/src/components/profiles/ProfileItem.js
+++ client/src/components/profiles/ProfileItem.js
@@ -13,7 +13,7 @@ const ProfileItem = ({
 }) => {
   return (
     <div className='profile bg-light'>
-      <img src='{avatar}' alt='avatar' className='round-img' />
+      <img src={avatar} alt='avatar' className='round-img' />
       <div>
         <h2>{name}</h2>
         <p>
