efb05fc0b07028294d65ac46ea39f9e4f8a37dce
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-16 00:18:09 -0500

    Fixed bug in profile action & added buttons to profile.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -50,7 +50,7 @@ export const getProfiles = () => async dispatch => {
 //Get profile by ID
 export const getProfileById = userId => async dispatch => {
   try {
-    const res = await axios.get(`api/profile/user/${userId}`);
+    const res = await axios.get(`/api/profile/user/${userId}`);
 
     dispatch({
       type: GET_PROFILE,
