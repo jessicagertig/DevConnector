8169a2e283a4bd66dfca889dcb9eb5aeb7f27f17
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-16 01:26:26 -0500

    Created ProfileAbout component and put in parent component.

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+const ProfileAbout = props => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+ProfileAbout.propTypes = {
+
+}
+
+export default ProfileAbout
