3fc5eda6b801805238168b97f9b6013d9bc22e72
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-16 01:37:06 -0500

    Conditionally added bio. Used trim and split method to extract first name only from username.

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -1,16 +1,36 @@
-import React from 'react'
+import React, { Fragment } from 'react'
 import PropTypes from 'prop-types'
 
-const ProfileAbout = props => {
+const ProfileAbout = ({ profile: {
+  bio,
+  skills,
+  user: { name }
+}}) => {
   return (
-    <div>
-      
+    <div class="profile-about bg-light p-2">
+      { bio && (
+        <Fragment>
+          <h2 class="text-primary">{name.trim().split(' ')[0]}'s Bio</h2>
+          <p>
+            {bio}
+          </p>
+          <div class="line"></div>
+        </Fragment>
+      )}
+      <h2 class="text-primary">Skill Set</h2>
+      <div class="skills">
+        <div class="p-1"><i class="fa fa-check"></i> HTML</div>
+        <div class="p-1"><i class="fa fa-check"></i> CSS</div>
+        <div class="p-1"><i class="fa fa-check"></i> JavaScript</div>
+        <div class="p-1"><i class="fa fa-check"></i> Python</div>
+        <div class="p-1"><i class="fa fa-check"></i> C#</div>
+      </div>
     </div>
   )
 }
 
 ProfileAbout.propTypes = {
-
+profile: PropTypes.object.isRequired,
 }
 
 export default ProfileAbout
