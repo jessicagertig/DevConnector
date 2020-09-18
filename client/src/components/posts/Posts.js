bad5cf073d2b00825015ab08bb4f859f6dc7d5b2
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-18 01:39:53 -0500

    Set up parent component to display/map over new component PostItem.

--- client/src/components/posts/Posts.js
+++ client/src/components/posts/Posts.js
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import { getPosts } from '../../actions/post'
 import Spinner from '../layout/Spinner'
+import PostItem from './PostItem'
 
 const Posts = ({ getPosts, post: { posts, loading }}) => {
 
@@ -10,11 +11,18 @@ const Posts = ({ getPosts, post: { posts, loading }}) => {
     getPosts()
   }, [getPosts])
 
-  return (
-    <div>
-      
+  return loading ? <Spinner /> : <Fragment>
+    <h1 className="large text-primary">Posts</h1>
+    <p className="lead">
+      <i className="fas fa-user"></i>Welcome to the community!
+    </p>
+    {/* PostForm */}
+    <div className="posts">
+      {posts.map(post => {
+        <PostItem key={post._id} post={post} />
+      })}
     </div>
-  )
+  </Fragment>
 }
 
 Posts.propTypes = {
