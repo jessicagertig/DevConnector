d8fad242b113fa65440c1456148442cb89db93c3
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-17 22:31:40 -0500

    Fix bugs. Start trying to fetch github repos. Encountering bug.

--- client/src/components/profile/ProfileAbout.js
+++ client/src/components/profile/ProfileAbout.js
@@ -9,20 +9,20 @@ const ProfileAbout = ({
   },
 }) => {
   return (
-    <div class='profile-about bg-light p-2'>
+    <div className='profile-about bg-light p-2'>
       {bio && (
         <Fragment>
           {/* name.trim().split(' ')[0] takes the user name and creates an array, split by spaces, of the first and last name of the user, then selects only first name which is at index 0 */}
-          <h2 class='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
+          <h2 className='text-primary'>{name.trim().split(' ')[0]}'s Bio</h2>
           <p>{bio}</p>
-          <div class='line'></div>
+          <div className='line'></div>
         </Fragment>
       )}
-      <h2 class='text-primary'>Skill Set</h2>
-      <div class='skills'>
+      <h2 className='text-primary'>Skill Set</h2>
+      <div className='skills'>
         {skills.map((skill, index) => (
-          <div key={index} class='p-1'>
-            <i class='fa fa-check'></i>
+          <div key={index} className='p-1'>
+            <i className='fa fa-check'></i>
             {skill}
           </div>
         ))}
--- client/src/components/profile/ProfileGithub.js
+++ client/src/components/profile/ProfileGithub.js
@@ -2,18 +2,18 @@ import React, { useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { connect } from 'react-redux'
 import Spinner from '../layout/Spinner'
-import { getGitHubRepos } from '../../profile'
+import { getGithubRepos } from '../../actions/profile'
 
 const ProfileGithub = ({ username, getGitHubRepos, repos }) => {
-  return (
-    <div>
-      
-    </div>
-  )
+
+  useEffect(() => {
+    getGitHubRepos(username)
+  }, [getGitHubRepos, username])
+  return <div />
 }
 
 ProfileGithub.propTypes = {
-  getGitHubRepos: PropTypes.func.isRequired,
+  getGithubRepos: PropTypes.func.isRequired,
   repos: PropTypes.array.isRequired,
   username: PropTypes.string.isRequired,
 }
@@ -22,4 +22,4 @@ const mapStateToProps = state => ({
   repos: state.profile.repos
 })
 
-export default connect(mapStateToProps, { getGitHubRepos })(ProfileGithub)
+export default connect(mapStateToProps, { getGithubRepos })(ProfileGithub)
