e0613d234d3983e3e3ac2b54c2cf7e4ea04042de
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-18 21:30:05 -0500

    Created UPDATE_LIKES type/reducer and addLikes & removeLikes actions.

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,31 +1,36 @@
-import {
-  GET_POSTS,
-  POST_ERROR
-} from '../actions/types'
+import { GET_POSTS, POST_ERROR, UPDATE_LIKES } from '../actions/types';
 
 const initialState = {
   posts: [],
   post: null,
   loading: true,
-  error: {}
-}
+  error: {},
+};
 
-export default function(state = initialState, action) {
+export default function (state = initialState, action) {
   const { type, payload } = action;
 
-  switch(type) {
+  switch (type) {
     case GET_POSTS:
       return {
         ...state,
         posts: payload,
-        loading: false
-      }
+        loading: false,
+      };
     case POST_ERROR:
       return {
         ...state,
         error: payload,
-        loading: false
-      }
+        loading: false,
+      };
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map(post =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false,
+      };
     default:
       return state;
   }
