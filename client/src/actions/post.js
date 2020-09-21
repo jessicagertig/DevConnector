e61e9f8ac24505f4669834fd40e42548cb345d29
Jessica Cooper <jessicagcooper@gmail.com>
2020-09-20 21:58:32 -0500

    Created ADD_COMMENT & REMOVE_COMMENT reducers & actions.

--- client/src/actions/post.js
+++ client/src/actions/post.js
@@ -7,6 +7,8 @@ import {
   DELETE_POST,
   ADD_POST,
   GET_POST,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
 } from './types';
 
 // Get posts
@@ -108,6 +110,44 @@ export const getPost = id => async dispatch => {
       type: GET_POST,
       payload: res.data,
     });
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Add Comment
+export const addComment = (postId, formData) => async dispatch => {
+  try {
+    const res = await axios.post(`/api/posts/${postId}/comment`, formData);
+
+    dispatch({
+      type: ADD_COMMENT,
+      payload: res.data, //will be comments array
+    });
+
+    dispatch(setAlert('Comment Added', 'success'));
+  } catch (err) {
+    dispatch({
+      type: POST_ERROR,
+      payload: { msg: err.response.statusText, status: err.response.status },
+    });
+  }
+};
+
+// Delete Comment
+export const removeComment = (postId, commentId) => async dispatch => {
+  try {
+    const res = await axios.delete(`/api/posts/${postId}/comment/${commentId}`);
+
+    dispatch({
+      type: REMOVE_COMMENT,
+      payload: commentId,
+    });
+
+    dispatch(setAlert('Comment Removed', 'success'));
   } catch (err) {
     dispatch({
       type: POST_ERROR,
